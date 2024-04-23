5a28f0fe518560b189fd01771b62fa8d3401c4fe
Som-Cillindr <140882309+Som-Cillindr@users.noreply.github.com>
2024-04-23 11:39:21 +1000

    ю
    .

---
+++ bobr.js
@@ -0,0 +1,8 @@
+let button_start = document.querySelector(".начать-б")
+
+button_start.addEventListener("click",=() =>{
+   function name(params) {
+    darkBlock.style.display  = "none"
+   }
+
+})
